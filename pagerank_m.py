4a5c528f63c997ce0b96e993b6106ad6348e770e
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-11-20 00:59:28 +0100

    Update pagerank_m.py

--- pagerank_m.py
+++ pagerank_m.py
@@ -2,43 +2,48 @@ import sys, re
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-class Node():
-    def __init__(self,id_n,AdjacencyList):
-        self.id = id_n
-        self.AdjacencyList = AdjacencyList
-
 class PageRank(MRJob):
     c = 0.15
     nIt = 2
     nodesInstances = {}
 
+    # Lecture du fichier et identification des couples source -> destination
     def mapper1(self,_, line):
-        #noeud -> noeud cité
         list = re.findall(r"\d+",str(line))
         if len(list)==2:
             [is_quoted_by,node] = list
             yield node, is_quoted_by
 
-    def reducer1(self, node, AdjacencyList):
-        """if node not in PageRank.nodesInstances:
-            PageRank.nodesInstances[node] = Node(node,[is_quoted_by])
-            yield node
-        else:
-            node = PageRank.nodesInstances.get(node)
-            node.AdjacencyList.append(is_quoted_by)"""
-        yield Node(node,AdjacencyList)
+    # Ajouts des noeuds au dictionnaire des instances de noeuds
+    def combiner(self, node, AdjacencyList):
+        tmp = set(AdjacencyList)
+        PageRank.nodesInstances[node] = {'rank':0,'AdjacencyList':tmp}
 
-    def mapper2(self, _, node):
-        new_rank = PageRank.c*node.rank
-        for neighbour in node.AdjacencyList:
-            neighbourInstance = PageRank.nodesInstances.get(neighbour)
-            new_rank += (1-PageRank.c)*neighbourInstance.rank/len(node.AdjacencyList)
-        node.rank = new_rank
-        yield node
+        for neighbour in tmp:
+            if neighbour not in PageRank.nodesInstances:
+                PageRank.nodesInstances[neighbour] = {'rank':0,'AdjacencyList':set()}      
+
+        yield node, None
+
+    def reducer1(self, node, _):
+        PageRank.nodesInstances[node]['rank'] = 1/len(PageRank.nodesInstances)
+        yield node, None
+
+    def mapper2(self, node, _):
+        new_rank = 0
+        for neighbour in PageRank.nodesInstances[node]['AdjacencyList']:
+                new_rank += (1-PageRank.c)*PageRank.nodesInstances[neighbour]['rank']
+
+        PageRank.nodesInstances[node]['rank'] = \
+            PageRank.c*PageRank.nodesInstances[node]['rank'] + \
+            new_rank/len(PageRank.nodesInstances[node]['AdjacencyList'])
+
+        yield node, PageRank.nodesInstances[node]['rank']
 
 
     def steps(self):
-        return [MRStep(mapper=self.mapper1,reducer=self.reducer1)] + PageRank.nIt * [MRStep(mapper=self.mapper2)]
+        return [MRStep(mapper=self.mapper1,combiner=self.combiner,reducer=self.reducer1)] +\
+            PageRank.nIt * [MRStep(mapper=self.mapper2)]
 
 if __name__ == '__main__':
     #./main.py file_path
